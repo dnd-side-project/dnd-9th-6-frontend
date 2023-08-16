11f3aa8b5233ad17c8977c1fe89f187ecb0d454e
김태강 <k_2684@naver.com>
2023-08-17 00:20:48 +0900

    Add pretendard font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import StyledThemeProvider from '@/components/providers/StyledThemeProvider';
 import React from 'react';
+import localFont from 'next/font/local';
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -14,5 +15,26 @@ const rootLayout = ({ children }: RootLayoutProps) => {
     </html>
   );
 };
+
+export const pretendard = localFont({
+  src: './fonts/PretendardVariable.woff2',
+  display: 'swap',
+  fallback: [
+    'Pretendard',
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'system-ui',
+    'Roboto',
+    'Helvetica Neue',
+    'Segoe UI',
+    'Apple SD Gothic Neo',
+    'Noto Sans KR',
+    'Malgun Gothic',
+    'Apple Color Emoji',
+    'Segoe UI Emoji',
+    'Segoe UI Symbol',
+    'sans-serif',
+  ],
+});
 
 export default rootLayout;
