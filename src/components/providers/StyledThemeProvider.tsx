652a27e32a7ff948c2cd2f80bcad54cd0e9c8b5a
김태강 <k_2684@naver.com>
2023-08-17 20:13:17 +0900

    Delete styledTheme

--- src/components/providers/StyledThemeProvider.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import { PropsWithChildren } from 'react';
-import { ThemeProvider } from 'styled-components';
-import theme from '@/styles/theme';
-
-export default function StyledThemeProvider({ children }: PropsWithChildren) {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
-}
