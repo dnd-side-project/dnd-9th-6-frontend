d7befb3a4dd4efc2869e451e1fe8521c27410e41
김태강 <k_2684@naver.com>
2023-08-18 15:19:47 +0900

    Add ChakraUI CacheProvider

--- src/components/providers/ChakraUIProvider.tsx
+++ src/components/providers/ChakraUIProvider.tsx
@@ -1,7 +1,16 @@
 'use client';
-import { PropsWithChildren } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
+import { ReactNode } from 'react';
 import theme from '@/styles/theme';
-export default function ChakraUIProvider({ children }: PropsWithChildren) {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+import { CacheProvider } from '@chakra-ui/next-js';
+export default function ChakraUIProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <CacheProvider>
+      <ChakraProvider theme={theme}>{children}</ChakraProvider>
+    </CacheProvider>
+  );
 }
