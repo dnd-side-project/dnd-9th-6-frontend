ec722d554cfde75ef90412ec4062d944d8e57a95
김태강 <k_2684@naver.com>
2023-08-20 04:00:22 +0900

    Add temporary Login Page

---
+++ src/app/(loginRoutes)/login/page.tsx
@@ -0,0 +1,12 @@
+//TODO: 로그인 페이지 구현
+
+const page = () => {
+  return (
+    <div>
+      <a href={process.env.NEXT_PUBLIC_GOOGLE_LOGIN_URL}>구글 로그인</a>
+      <a href={process.env.NEXT_PUBLIC_KAKAO_LOGIN_URL}>카카오 로그인</a>
+    </div>
+  );
+};
+
+export default page;
