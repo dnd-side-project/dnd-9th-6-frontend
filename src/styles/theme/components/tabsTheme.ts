a53a682ddc3da87ff6a63ee95e4740c9195e42d3
김태강 <k_2684@naver.com>
2023-08-25 18:35:00 +0900

    Enhance Tabs Component

--- src/styles/theme/components/tabsTheme.ts
+++ src/styles/theme/components/tabsTheme.ts
@@ -1,32 +1,27 @@
-import { defineStyle, defineStyleConfig } from '@chakra-ui/react';
+import { defineStyleConfig } from '@chakra-ui/react';
 import { typo } from '../foundations/typo';
-
-const tabsVariant = defineStyle(props => {
-  const { colorScheme } = props;
-  return {
-    tab: {
-      color: 'grayscale.white',
-      opacity: 0.7,
-      py: '16px',
-      px: '24px',
-      ...typo.body3.bold,
-      _selected: {
-        opacity: 1,
-        borderBottom: '2px solid',
-        borderColor: 'grayscale.white',
-      },
-    },
-    tablist: {
-      display: 'inline-flex',
-      bg: `${colorScheme}`,
-      gap: '16px',
-    },
-  };
-});
 
 export const tabsTheme = defineStyleConfig({
   variants: {
-    tabsVariant,
+    tabsVariant: {
+      tab: {
+        color: 'grayscale.white',
+        opacity: 0.7,
+        py: '16px',
+        px: '24px',
+        ...typo.body3.bold,
+        _selected: {
+          opacity: 1,
+          borderBottom: '2px solid',
+          borderColor: 'grayscale.white',
+        },
+      },
+      tablist: {
+        display: 'inline-flex',
+        bg: 'transparent',
+        gap: '16px',
+      },
+    },
   },
 
   defaultProps: {
