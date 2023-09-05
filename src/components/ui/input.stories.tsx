df07bb8b13911db1fa6e2b146baab636cf264d2c
김태강 <k_2684@naver.com>
2023-09-05 18:29:48 +0900

    Implement Input Component

---
+++ src/components/ui/input.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Input } from './input';
+import Search from 'assets/icons/search.svg';
+
+const meta = {
+  title: 'Classcope/Input',
+  component: Input,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Input>;
+
+export default meta;
+
+export const SearchInput: StoryObj<typeof Input> = {
+  args: {
+    variant: 'search',
+    size: 'lg',
+    placeholder: '검색해보세요',
+  },
+  render: args => (
+    <div className="relative w-[695px]">
+      <Input {...args} />
+      <button className="absolute right-0 top-0 flex h-full w-[56px] border-l border-grayscale-200 p-16">
+        <div className="translate-y-[-2px]">
+          <Search />
+        </div>
+      </button>
+    </div>
+  ),
+};
