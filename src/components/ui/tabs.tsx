d9774c1b843ac28a4325a4a590b0f65de9966108
김태강 <k_2684@naver.com>
2023-09-05 15:04:34 +0900

    Implement Tabs Component

---
+++ src/components/ui/tabs.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import * as React from 'react';
+import * as TabsPrimitive from '@radix-ui/react-tabs';
+
+import { cn } from 'utils/twUtils';
+
+const Tabs = TabsPrimitive.Root;
+
+const TabsList = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.List>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
+>(({ className, ...props }, ref) => (
+  <TabsPrimitive.List
+    ref={ref}
+    className={cn('inline-flex gap-16', className)}
+    {...props}
+  />
+));
+TabsList.displayName = TabsPrimitive.List.displayName;
+
+const TabsTrigger = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
+>(({ className, ...props }, ref) => (
+  <TabsPrimitive.Trigger
+    ref={ref}
+    className={cn(
+      'z-6 inline-flex items-center justify-center px-24 py-16 text-white/70 transition-all body3-semibold data-[state=active]:border-b-[2px] data-[state=active]:text-white',
+      className
+    )}
+    {...props}
+  />
+));
+TabsTrigger.displayName = TabsPrimitive.Trigger.displayName;
+
+export { Tabs, TabsList, TabsTrigger };
