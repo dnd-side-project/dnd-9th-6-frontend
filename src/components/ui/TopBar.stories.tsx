93b2a5db2b62ac68065aceb11b6d12d6d507fe0b
김태강 <k_2684@naver.com>
2023-09-07 17:51:40 +0900

    Add TopBar Stories

---
+++ src/components/ui/TopBar.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import TopBar from './TopBar';
+
+const meta = {
+  title: 'Classcope/TopBar',
+  component: TopBar,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+} satisfies Meta<typeof TopBar>;
+
+export default meta;
+
+export const TopBarExample: StoryObj<typeof TopBar> = {
+  render: () => <TopBar />,
+};
