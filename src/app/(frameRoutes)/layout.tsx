f1a2b19698800ec5e40408162c242346d414cabe
김태강 <k_2684@naver.com>
2023-09-07 17:54:04 +0900

    Replace Topbar&Footer components

--- src/app/(frameRoutes)/layout.tsx
+++ src/app/(frameRoutes)/layout.tsx
@@ -1,4 +1,4 @@
-import Footer from './components/Footer';
+import Footer from 'components/ui/Footer';
 import TopBar from 'components/ui/TopBar';
 
 interface RootLayoutProps {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Home from 'pages/Home';
-import Footer from './(frameRoutes)/components/Footer';
 import TopBar from 'components/ui/TopBar';
+import Footer from 'components/ui/Footer';
 
 const page = () => {
   return (
     <div className="relative flex h-full w-full flex-col">
       <TopBar />
-      <div className="m-auto w-full flex-1 flex-col bg-bg">
+      <div className="w-full">
         <Home />
       </div>
       <Footer />
