351f4a61e63cba5bd259f4e4e5c21aef5e3f9cf5
김태강 <k_2684@naver.com>
2023-09-07 17:55:35 +0900

    Add Card Component Stories

--- src/components/ui/card.stories.tsx
+++ src/components/ui/card.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Card } from './card';
-import { CoverCard, OutlinedCard } from 'components/Card';
+import { CoverCard, OutlinedCard, LandScapeCard } from 'components/Card';
 
 const meta = {
   title: 'Classcope/Card',
@@ -31,4 +31,17 @@ export const OutlinedCardExample: StoryObj<typeof OutlinedCard> = {
       'https://storage.googleapis.com/static.fastcampus.co.kr/prod/uploads/202308/132817-865/thumb--1-.jpg',
   },
   render: args => <OutlinedCard {...args} />,
+};
+
+export const LandScapeCardExample: StoryObj<typeof LandScapeCard> = {
+  args: {
+    강사: '김코딩',
+    타이틀: '김코딩의 자바스크립트 강의',
+    가격: '100,000',
+    플랫폼: 'fastcampus',
+    이미지:
+      'https://storage.googleapis.com/static.fastcampus.co.kr/prod/uploads/202308/132817-865/thumb--1-.jpg',
+    후기수: 100,
+  },
+  render: args => <LandScapeCard {...args} />,
 };
