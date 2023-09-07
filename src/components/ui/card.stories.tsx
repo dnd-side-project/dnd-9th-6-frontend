d4d9b0e788ae5492884612594f78af8015ece03b
김태강 <k_2684@naver.com>
2023-09-08 00:53:11 +0900

    Add Card Stories

--- src/components/ui/card.stories.tsx
+++ src/components/ui/card.stories.tsx
@@ -1,5 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { CoverCard, OutlinedCard, LandScapeCard } from 'components/Card';
+import {
+  CoverCard,
+  OutlinedCard,
+  LandScapeCard,
+  HorizontalCard,
+  SquareCard,
+} from 'components/Card';
 import { Card } from './card';
 
 const meta = {
@@ -44,4 +50,32 @@ export const LandScapeCardExample: StoryObj<typeof LandScapeCard> = {
     후기수: 100,
   },
   render: args => <LandScapeCard {...args} />,
+};
+export const HorizontalCardardExample: StoryObj<typeof HorizontalCard> = {
+  args: {
+    타이틀: '김코딩의 자바스크립트 강의',
+    작성자: '김코딩',
+    별점: 4.5,
+    작성일: '2023-08-23T00:00:00',
+    내용: '강의가 너무 좋아요',
+    태그: '뛰어난 강의력,구성이 알차요,도움이 많이 됐어요',
+    플랫폼: 'fastcampus',
+  },
+  render: args => <HorizontalCard {...args} />,
+};
+
+export const SquareCardExample: StoryObj<typeof SquareCard> = {
+  args: {
+    작성자: '김코딩',
+    별점: 4.5,
+    작성일: '2023-08-23T00:00:00',
+    내용: '강의가 너무 좋아요',
+    태그: '뛰어난 강의력,구성이 알차요,도움이 많이 됐어요',
+    이미지:
+      'https://storage.googleapis.com/static.fastcampus.co.kr/prod/uploads/202308/132817-865/thumb--1-.jpg',
+    플랫폼: 'fastcampus',
+    찜수: 100,
+    좋아요: true,
+  },
+  render: args => <SquareCard {...args} />,
 };
