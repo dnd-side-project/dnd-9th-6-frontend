2d2575cb4ea0dffc75752bd2194dec5c730f2826
김태강 <k_2684@naver.com>
2023-09-12 01:01:39 +0900

    Refactor radio-group style

--- src/components/ui/radio-group.tsx
+++ src/components/ui/radio-group.tsx
@@ -11,7 +11,7 @@ const RadioGroup = React.forwardRef<
 >(({ className, ...props }, ref) => {
   return (
     <RadioGroupPrimitive.Root
-      className={cn('flex gap-8', className)}
+      className={cn('flex max-w-[660px] flex-wrap gap-8', className)}
       {...props}
       ref={ref}
     />
@@ -27,7 +27,7 @@ const RadioGroupItem = React.forwardRef<
     <RadioGroupPrimitive.Item
       ref={ref}
       className={cn(
-        'flex max-h-[34px] appearance-none items-center justify-center rounded-[2px] border-[1px] border-grayscale-100 bg-white px-16 py-8 text-grayscale-800 body3-bold body3-medium disabled:pointer-events-none disabled:opacity-50 data-[state=checked]:border-blue-400 data-[state=checked]:text-blue-500 data-[state=checked]:body3-semibold',
+        'flex appearance-none items-center justify-center whitespace-nowrap rounded-[2px] border-[1px] border-grayscale-100 bg-white px-16 py-8 text-grayscale-800 body3-medium disabled:pointer-events-none disabled:opacity-50 data-[state=checked]:border-blue-400 data-[state=checked]:text-blue-500 data-[state=checked]:body3-semibold',
         className
       )}
       {...props}
