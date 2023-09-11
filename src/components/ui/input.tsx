5c975c1555fbdf9518d4276094ca15028a983b01
김태강 <k_2684@naver.com>
2023-09-11 19:26:57 +0900

    Refactor Input variant Style

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,15 +3,15 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from 'utils/twUtils';
 
 const inputVariant = cva(
-  'flex w-full rounded-[2px] border border-grayscale-50 bg-white body2-medium file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-grayscale-300 focus-visible:shadow-blue focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-400 disabled:cursor-not-allowed disabled:opacity-50',
+  'flex w-full rounded-[2px] border border-grayscale-50 bg-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-grayscale-300 focus-visible:shadow-blue focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-400 disabled:cursor-not-allowed disabled:opacity-50',
   {
     variants: {
       variant: {
-        search: 'h-[48px] shadow-main',
+        search: 'shadow-main',
       },
       size: {
-        sm: 'p-10',
-        lg: 'p-16',
+        sm: 'h-[32px] p-8',
+        lg: 'h-[48px] p-16 ',
       },
     },
     defaultVariants: {
