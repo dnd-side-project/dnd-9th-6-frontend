698dd36340eeaf6ae1b720f45512b58f101ea8d0
김태강 <k_2684@naver.com>
2023-09-12 13:19:21 +0900

    Refactor Accordion font Style

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -47,7 +47,7 @@ const AccordionContent = React.forwardRef<
   <AccordionPrimitive.Content
     ref={ref}
     className={cn(
-      'overflow-hidden transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down',
+      'overflow-hidden transition-all body3-semibold data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down',
       className
     )}
     {...props}
