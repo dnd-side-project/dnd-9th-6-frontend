948ec983f01a6b1db9b34af8526f3b1d53071e82
김태강 <k_2684@naver.com>
2023-09-17 00:05:03 +0900

    Add Lectures InfiniteQuery

--- src/hooks/reactQuery/lectures/query.ts
+++ src/hooks/reactQuery/lectures/query.ts
@@ -1,18 +1,39 @@
-import { useQuery, UseQueryOptions } from '@tanstack/react-query';
-import lecturesApi from 'apis/lectures';
+import {
+  useInfiniteQuery,
+  UseInfiniteQueryOptions,
+  useQuery,
+  UseQueryOptions,
+} from '@tanstack/react-query';
+import LECTURES_API from 'apis/lectures';
 import { LecturesParams, LecturesResponse } from 'apis/lectures/types';
 import { AxiosError } from 'axios';
 import { LECTURES_KEY } from 'constants/querykeys';
 
-export const useGetLecturesParameter = (
-  params?: LecturesParams['lecturesParameter'],
-  options?: UseQueryOptions<LecturesResponse['search'], AxiosError>
+export const useGetLectures = (
+  params?: LecturesParams['get'],
+  options?: UseQueryOptions<LecturesResponse, AxiosError>
 ) => {
-  return useQuery<LecturesResponse['search'], AxiosError>(
+  return useQuery<LecturesResponse, AxiosError>(
     LECTURES_KEY.list([{ ...params }]),
-    () => lecturesApi.search(params),
+    () => LECTURES_API.search(params),
     {
       ...options,
     }
   );
 };
+
+export const useGetInfiniteLectures = (
+  params?: LecturesParams['get'],
+  options?: UseInfiniteQueryOptions<LecturesResponse, AxiosError>
+) =>
+  useInfiniteQuery<LecturesResponse, AxiosError>(
+    LECTURES_KEY.list([{ ...params }]),
+    ({ pageParam = 1 }) => LECTURES_API.search({ ...params, page: pageParam }),
+    {
+      getNextPageParam: ({ totalPages, pageNumber }) => {
+        const nextPage = pageNumber + 1;
+        return totalPages > pageNumber ? nextPage : undefined;
+      },
+      ...options,
+    }
+  );
