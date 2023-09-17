c6a6a197a90344396a9a670febe74412400217d7
김태강 <k_2684@naver.com>
2023-09-17 17:42:25 +0900

    Fix Lecutre page&size param value

--- src/app/(frameRoutes)/lectures/page.tsx
+++ src/app/(frameRoutes)/lectures/page.tsx
@@ -37,8 +37,7 @@ const Lectures = () => {
     mainCategoryId: mainCategoryIdValue,
     subCategoryId: subCategoryIdValue,
     searchKeyword: searchKeywordValue,
-    page: searchParams.get('page') ?? 0,
-    size: searchParams.get('size') ?? 8,
+    size: 10,
     sort: searchParams.get('sort') ?? '',
   };
 
