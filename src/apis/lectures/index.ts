a1ad5c7dfb5074c5bc1d57133f3f7773d429afe4
김태강 <k_2684@naver.com>
2023-10-30 00:04:47 +0900

    Refactor Lecture params

--- src/apis/lectures/index.ts
+++ src/apis/lectures/index.ts
@@ -1,16 +1,13 @@
-import { objToQueryString } from 'utils/objToQueryString';
-import isEmpty from 'lodash/isEmpty';
 import instance from '..';
 import { LecturesParams, LecturesResponse } from './types';
 
 const LECTURES_API_URL = '/lectures';
 
 const LECTURES_API = {
-  search: async (params?: LecturesParams['get']) => {
-    const queryString = isEmpty(params) ? '' : `?${objToQueryString(params)}`;
-    return instance.get<LecturesResponse, LecturesResponse>(
-      LECTURES_API_URL + queryString
-    );
+  search: async (params?: LecturesParams) => {
+    return instance.get<LecturesResponse>(LECTURES_API_URL, {
+      params,
+    });
   },
 };
 
