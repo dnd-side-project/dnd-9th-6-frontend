ce4c982306dd94ea50336f2b38b51d2e11e236d4
김태강 <k_2684@naver.com>
2023-10-30 00:35:54 +0900

    Refactor Scope Query Type

--- src/hooks/reactQuery/scope/query.ts
+++ src/hooks/reactQuery/scope/query.ts
@@ -1,35 +1,51 @@
 import { useQuery, UseQueryOptions } from '@tanstack/react-query';
 import SCOPE_API from 'apis/scope';
-import { ScopeResponse } from 'apis/scope/types';
-import { AxiosError } from 'axios';
+import {
+  ScopeLecturesResponse,
+  ScopeRecentResponse,
+  ScopeReviewsResponse,
+} from 'apis/scope/types';
+import { AxiosError, AxiosResponse } from 'axios';
 import { SCOPE_KEY } from 'constants/querykeys';
 
 export const useGetScopeReviews = (
-  options?: UseQueryOptions<ScopeResponse['reviews'], AxiosError>
+  options?: UseQueryOptions<
+    AxiosResponse<ScopeReviewsResponse>,
+    AxiosError,
+    ScopeReviewsResponse
+  >
 ) => {
-  return useQuery<ScopeResponse['reviews'], AxiosError>(
-    SCOPE_KEY.list(['reviews']),
-    () => SCOPE_API.reviews(),
-    { ...options }
-  );
+  return useQuery<
+    AxiosResponse<ScopeReviewsResponse>,
+    AxiosError,
+    ScopeReviewsResponse
+  >(SCOPE_KEY.list(['reviews']), () => SCOPE_API.reviews(), { ...options });
 };
 
 export const useGetScopeLectures = (
-  options?: UseQueryOptions<ScopeResponse['lectures'], AxiosError>
+  options?: UseQueryOptions<
+    AxiosResponse<ScopeLecturesResponse>,
+    AxiosError,
+    ScopeLecturesResponse
+  >
 ) => {
-  return useQuery<ScopeResponse['lectures'], AxiosError>(
-    SCOPE_KEY.list(['lectures']),
-    () => SCOPE_API.lectures(),
-    { ...options }
-  );
+  return useQuery<
+    AxiosResponse<ScopeLecturesResponse>,
+    AxiosError,
+    ScopeLecturesResponse
+  >(SCOPE_KEY.list(['lectures']), () => SCOPE_API.lectures(), { ...options });
 };
 
 export const useGetScopeRecent = (
-  options?: UseQueryOptions<ScopeResponse['recent'], AxiosError>
+  options?: UseQueryOptions<
+    AxiosResponse<ScopeRecentResponse>,
+    AxiosError,
+    ScopeRecentResponse
+  >
 ) => {
-  return useQuery<ScopeResponse['recent'], AxiosError>(
-    SCOPE_KEY.list(['recent']),
-    () => SCOPE_API.recent(),
-    { ...options }
-  );
+  return useQuery<
+    AxiosResponse<ScopeRecentResponse>,
+    AxiosError,
+    ScopeRecentResponse
+  >(SCOPE_KEY.list(['recent']), () => SCOPE_API.recent(), { ...options });
 };
