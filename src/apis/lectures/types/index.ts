cc416cc9db80866f2d993e8202368ceb900f5f78
김태강 <k_2684@naver.com>
2023-10-30 00:07:26 +0900

    Extend GlobalResponse Type to Lecutre Type

--- src/apis/lectures/types/index.ts
+++ src/apis/lectures/types/index.ts
@@ -1,30 +1,32 @@
-export type LecturesParams = {
-  get: {
-    mainCategoryId?: number | string;
-    subCategoryId?: number | string;
-    searchKeyword?: string;
-    page?: number | string;
-    size?: number | string;
-    sort?: string;
-  };
-};
+export interface Lecture {
+  id: number;
+  title: string;
+  source: string;
+  url: string;
+  price: string;
+  name: string;
+  mainCategory: string;
+  subCategory: string;
+  imageUrl: string;
+  reviewCount: number;
+  bookmarkCount: number;
+}
 
-export type LecturesResponse = {
-  totalPages: number;
-  pageNumber: number;
-  pageSize: number;
-  totalElements: number;
-  lectures: {
-    id: number;
-    title: string;
-    source: string;
-    url: string;
-    price: string;
-    name: string;
-    mainCategory: string;
-    subCategory: string;
-    imageUrl: string;
-    reviewCount: number;
-    bookmarkCount: number;
-  }[];
-};
+export interface LecturesParams {
+  mainCategoryId?: number | string;
+  subCategoryId?: number | string;
+  searchKeyword?: string;
+  page?: number | string;
+  size?: number | string;
+  sort?: string;
+}
+
+export interface LecturesResponse extends GlobalResponse {
+  data: {
+    totalPages: number;
+    pageNumber: number;
+    pageSize: number;
+    totalElements: number;
+    lectures: Lecture[];
+  };
+}
