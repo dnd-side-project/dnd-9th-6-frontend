d777c915dd0630bd30ca379edcf172e76d97a76e
김태강 <k_2684@naver.com>
2023-11-01 02:59:26 +0900

    Implement keep Login logic at template

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -2,9 +2,36 @@
 
 import OnlyPCSupport from 'components/OnlyPCSupport/OnlyPCSupport';
 import { useMediaQuery } from 'hooks/useMediaQuery';
+import { USER_ACCESS_TOKEN } from 'constants/account';
+import { useAuthActions } from 'auth/store';
+import { useUserActions } from 'auth/store/user';
+import { useEffect } from 'react';
+import userApi from 'apis/user';
+import { getLocalStorage } from 'hooks/storage';
 
 export default function Template({ children }: { children: React.ReactNode }) {
   const isNotPC = useMediaQuery('(max-width: 1024px)');
+  const accessToken = getLocalStorage(USER_ACCESS_TOKEN);
+  const { setIsSignedIn, setIsRequesting } = useAuthActions();
+  const { setUserInfo } = useUserActions();
+
+  // accessToken으로 유저 정보를 가져오는 함수
+  useEffect(() => {
+    if (!accessToken) return;
+
+    (async () => {
+      try {
+        const userInfo = await userApi.get();
+        setUserInfo({
+          name: userInfo.data.data.nickName,
+        });
+        setIsRequesting(false);
+        setIsSignedIn(true);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, [accessToken, setIsRequesting, setIsSignedIn, setUserInfo]);
 
   return (
     <>
