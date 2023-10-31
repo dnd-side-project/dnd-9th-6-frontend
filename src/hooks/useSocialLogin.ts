ac1b1c4b67803d3e7c8f20656750cc5e6dfd8b3d
김태강 <k_2684@naver.com>
2023-11-01 02:44:04 +0900

    Refactor useSocialLogin Logic to use Storage

--- src/hooks/useSocialLogin.ts
+++ src/hooks/useSocialLogin.ts
@@ -4,8 +4,11 @@ import { useRouter } from 'next/navigation';
 
 import authApi from 'apis/auth';
 import { ROUTES } from 'constants/routes';
-import { USER_ID, USER_TOKEN } from 'constants/account';
+import { USER_ACCESS_TOKEN, USER_REFRESH_TOKEN } from 'constants/account';
 import { useMutation } from '@tanstack/react-query';
+import { useUserActions } from 'auth/store/user';
+import { useIsSignedIn } from 'auth/store';
+import { setLocalStorage, setSessionStorage } from './storage';
 
 type SocialLoginProps = {
   platform: string;
@@ -14,12 +17,16 @@ type SocialLoginProps = {
 
 const useSocialLogin = () => {
   const router = useRouter();
-
+  const isSignedIn = useIsSignedIn();
+  const { setUserInfo } = useUserActions();
   const { mutate: signIn, isLoading: isSignInLoading } = useMutation(
     authApi.signIn,
     {
       onSuccess: ({ data: { data } }) => {
         const { accessToken, refreshToken, ...userInfoData } = data;
+        setUserInfo(userInfoData);
+        setLocalStorage(USER_ACCESS_TOKEN, accessToken);
+        setSessionStorage(USER_REFRESH_TOKEN, refreshToken);
         router.push(ROUTES.HOME);
       },
       onError: () => {
@@ -28,8 +35,12 @@ const useSocialLogin = () => {
     }
   );
 
+  const signOut = () => authApi.signOut();
+
   return {
     signIn,
+    signOut,
+    isSignedIn,
     isSignInLoading,
   };
 };
