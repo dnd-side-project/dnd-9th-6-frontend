2180d55e508339ee2ee605a653c3bae8eee7ce41
김태강 <k_2684@naver.com>
2023-11-01 02:45:06 +0900

    Add auth api & user api

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,3 +1,12 @@
 export const AUTH_API = {
   SIGN_IN: '/auth/signin',
+  REISSUE: '/auth/reissue',
+  WITHDRAW: '/auth/withdraw',
+  SIGN_OUT: '/auth/signout',
+} as const;
+
+export const USER_API = {
+  GET_USER_INFO: '/auth',
+  UPDATE_USER_INFO: '/auth',
+  POST_INTERESTS: '/auth',
 } as const;
