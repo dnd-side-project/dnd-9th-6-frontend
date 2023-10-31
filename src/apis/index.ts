4c92403d3a1d2543d2c01a4670f61480862949c7
김태강 <k_2684@naver.com>
2023-11-01 02:49:38 +0900

    Refactor api instance logic

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,7 +1,16 @@
+import { authStore } from 'auth/store';
 import axios, { AxiosError, AxiosResponse } from 'axios';
+import { USER_ACCESS_TOKEN, USER_REFRESH_TOKEN } from 'constants/account';
 
 import { HTTP_BASE_URL } from 'constants/http';
 import { ROUTES } from 'constants/routes';
+import { getLocalStorage, getSessionStorage } from 'hooks/storage';
+import {
+  isAccessTokenExpired,
+  isRefreshTokenExpired,
+  isTokenNotExist,
+} from 'utils/http';
+import authApi from './auth';
 
 const instance = axios.create({
   baseURL: HTTP_BASE_URL,
@@ -45,25 +54,45 @@ instance.interceptors.response.use(
 
   async (error: AxiosError<GlobalResponse>) => {
     const { response } = error;
-    const { code, message } = response?.data || {};
+    const { code } = response?.data || {};
+    /**
+     * @description Access Token이 만료될 경우 Refresh Token으로 재발급하여 api 요청을 그대로 진행
+     */
+
+    if (isAccessTokenExpired() && !isRefreshTokenExpired()) {
+      const refreshToken = getSessionStorage(USER_REFRESH_TOKEN);
+      await authApi.reIssue(refreshToken as string);
+      return axios(response?.config!);
+    }
+
+    /**
+     * @description  토큰이 존재하지 않을 경우
+     */
+    if (isTokenNotExist()) {
+      const { setIsTokenRequired, setIsSignedIn } =
+        authStore.getState().actions;
+      setIsTokenRequired(true);
+      setIsSignedIn(false);
+    }
 
     // 강의 id가 없는 경우, 존재하지 않는 강의, (메인, 서브)카테고리,
-    if (
-      code === -1 ||
-      code === -2000 ||
-      code === -2001 ||
-      code === -2002 ||
-      code === -2003
-    ) {
-      alert(message);
+    if (code === -2000 || code === -2001 || code === -2002 || code === -2003) {
       window.location.replace(`${ROUTES.LECTURES}`);
     }
+
     // 존재하지 않는 사용자인 경우
     if (code === -1000) {
       window.location.replace(`${ROUTES.LOGIN}`);
     }
+
     return Promise.reject(error);
   }
 );
+
+const accessToken = getLocalStorage(USER_ACCESS_TOKEN);
+
+if (accessToken) {
+  instance.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+}
 
 export default instance;
