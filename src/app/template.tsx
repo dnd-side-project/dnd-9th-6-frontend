6a4d4d19a38fef1b2a89df64ed112c60824fe8e6
김태강 <k_2684@naver.com>
2023-11-05 03:21:05 +0900

    Refactor template Login logic

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -2,9 +2,8 @@
 
 import OnlyPCSupport from 'components/OnlyPCSupport/OnlyPCSupport';
 import { useMediaQuery } from 'hooks/useMediaQuery';
-import { USER_ACCESS_TOKEN } from 'constants/account';
+import { USER_ACCESS_TOKEN, USER_INFO } from 'constants/account';
 import { useEffect } from 'react';
-import userApi from 'apis/user';
 import { getLocalStorage } from 'hooks/storage';
 import { useAuthActions } from 'store/auth';
 import { useUserActions } from 'store/user';
@@ -12,26 +11,19 @@ import { useUserActions } from 'store/user';
 export default function Template({ children }: { children: React.ReactNode }) {
   const isNotPC = useMediaQuery('(max-width: 1024px)');
   const accessToken = getLocalStorage(USER_ACCESS_TOKEN);
+  const user = getLocalStorage(USER_INFO);
   const { setIsSignedIn, setIsRequesting } = useAuthActions();
   const { setUserInfo } = useUserActions();
 
-  // accessToken으로 유저 정보를 가져오는 함수
   useEffect(() => {
-    if (!accessToken) return;
-
-    (async () => {
-      try {
-        const userInfo = await userApi.get();
-        setUserInfo({
-          name: userInfo.data.data.nickName,
-        });
-        setIsRequesting(false);
-        setIsSignedIn(true);
-      } catch (error) {
-        console.error(error);
-      }
-    })();
-  }, [accessToken, setIsRequesting, setIsSignedIn, setUserInfo]);
+    if (user && accessToken) {
+      setUserInfo({ ...JSON.parse(user) });
+      setIsRequesting(false);
+      setIsSignedIn(true);
+      return;
+    }
+    setIsRequesting(false);
+  }, [accessToken, setIsRequesting, setIsSignedIn, setUserInfo, user]);
 
   return (
     <>
