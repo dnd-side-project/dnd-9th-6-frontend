60d45fade0c543bbd812fd03c9e6e17a647e6799
김태강 <k_2684@naver.com>
2023-11-05 03:17:00 +0900

    Add user query key

--- src/constants/querykeys.ts
+++ src/constants/querykeys.ts
@@ -3,3 +3,5 @@ import { createQueryKeys } from '../utils/createQueryKeys';
 export const SCOPE_KEY = createQueryKeys('scope');
 
 export const LECTURES_KEY = createQueryKeys('lectures');
+
+export const USER_KEY = createQueryKeys('user');
