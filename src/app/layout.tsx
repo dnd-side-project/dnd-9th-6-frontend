0f5676160a79b3ecc1575bf17e1c7714c0ec7d21
김태강 <k_2684@naver.com>
2023-11-18 00:46:53 +0900

    Fix PretendardVariable font in Safari

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,53 @@ interface RootLayoutProps {
 }
 
 const pretendard = localFont({
-  src: './fonts/PretendardVariable.woff2',
+  src: [
+    {
+      path: '/fonts/Pretendard-Thin.woff2',
+      weight: '100',
+      style: 'woff2',
+    },
+    {
+      path: '/fonts/Pretendard-ExtraLight.woff2',
+      weight: '200',
+      style: 'woff2',
+    },
+    {
+      path: '/fonts/Pretendard-Light.woff2',
+      weight: '300',
+      style: 'woff2',
+    },
+    {
+      path: '/fonts/Pretendard-Regular.woff2',
+      weight: '400',
+      style: 'woff2',
+    },
+    {
+      path: '/fonts/Pretendard-Medium.woff2',
+      weight: '500',
+      style: 'woff2',
+    },
+    {
+      path: '/fonts/Pretendard-Semibold.woff2',
+      weight: '600',
+      style: 'woff2',
+    },
+    {
+      path: '/fonts/Pretendard-Bold.woff2',
+      weight: '700',
+      style: 'woff2',
+    },
+    {
+      path: '/fonts/Pretendard-ExtraBold.woff2',
+      weight: '800',
+      style: 'woff2',
+    },
+    {
+      path: '/fonts/Pretendard-Black.woff2',
+      weight: '900',
+      style: 'woff2',
+    },
+  ],
   display: 'swap',
   fallback: [
     'Pretendard',
@@ -57,7 +103,7 @@ const rootLayout = ({ children }: RootLayoutProps) => {
   return (
     <html lang="ko">
       <link rel="icon" href="/favicon.ico" sizes="any" />
-      <body className={pretendard.className}>
+      <body className={pretendard.className}  >
         <AppProvider>{children}</AppProvider>
         <GoogleAnalytics />
       </body>
