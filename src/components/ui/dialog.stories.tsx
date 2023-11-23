c12d8b55500d4a369a8b315dd828806b42fe02b6
Taekang Kim <k_2684@naver.com>
2023-11-23 22:59:50 +0900

    Add button to LectureDialog component

--- src/components/ui/dialog.stories.tsx
+++ src/components/ui/dialog.stories.tsx
@@ -2,6 +2,7 @@ import LectureDialog from 'components/Dialog/LectureDialog';
 
 import type { Meta, StoryObj } from '@storybook/react';
 
+import { Button } from './button';
 import { Dialog } from './dialog';
 
 const meta = {
@@ -49,5 +50,9 @@ export const DialogExample: StoryObj<typeof LectureDialog> = {
       },
     ],
   },
-  render: (args) => <LectureDialog {...args} />,
+  render: (args) => (
+    <LectureDialog {...args}>
+      <Button variant="primary">Dialog</Button>
+    </LectureDialog>
+  ),
 };
