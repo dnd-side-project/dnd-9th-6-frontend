e700b6089ac6dfd9b9b4336ba9244da92f00c3b5
Taekang Kim <k_2684@naver.com>
2023-11-23 22:57:41 +0900

    Update z-index in DialogOverlay component

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -19,7 +19,7 @@ const DialogOverlay = React.forwardRef<
   <DialogPrimitive.Overlay
     ref={ref}
     className={cn(
-      'z-50 fixed inset-0 bg-black/50 backdrop-brightness-50 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+      'fixed inset-0 z-[999] bg-black/50 backdrop-brightness-50 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
       className
     )}
     {...props}
