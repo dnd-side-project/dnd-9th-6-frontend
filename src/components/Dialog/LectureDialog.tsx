7ededb34cfd60499bc50076985101bc67db8d567
Taekang Kim <k_2684@naver.com>
2023-11-23 22:58:51 +0900

    Refactoring LectureDialog

--- src/components/Dialog/LectureDialog.tsx
+++ src/components/Dialog/LectureDialog.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Image from 'next/image';
 import Bookmark2 from 'assets/icons/glass/bookmark.svg';
 import Career from 'assets/icons/glass/career.svg';
@@ -12,7 +13,9 @@ import { ScrollArea } from 'components/ui/scroll-area';
 import { Bookmark, MoveDownRightIcon, PencilIcon } from 'lucide-react';
 import { Rating } from 'react-simple-star-rating';
 
-interface LectureDialogProps {
+import * as DialogPrimitive from '@radix-ui/react-dialog';
+
+interface LectureDialogProps extends React.ComponentPropsWithoutRef<typeof DialogPrimitive.Root> {
   강사명: string;
   강의명: string;
   가격: string;
@@ -29,182 +32,194 @@ interface LectureDialogProps {
     태그: string;
     플랫폼: string;
   }[];
+  태그그룹: {
+    태그이름: string;
+    태그수: number;
+  }[][];
+  contentProps?: React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content>;
 }
 
-function LectureDialog({
-  강사명 = '둡',
-  강의명 = '연필 하나만으로 모든 분위기를 담아내요, 둡의 연필 드로잉',
-  가격 = '100,000원',
-  플랫폼 = 'fastcampus',
-  이미지 = '',
-  별점 = 4.5,
-  리뷰수 = 100,
-  추천후기 = [],
-  ...props
-}: LectureDialogProps) {
-  return (
-    <Dialog defaultOpen {...props}>
-      <DialogTrigger asChild>
-        <Button variant="primary">Share</Button>
-      </DialogTrigger>
-      <DialogContent className="flex h-[534px] w-[837px] flex-col bg-gray-100">
-        <DialogHeader className="flex h-[91px] w-full bg-white">
-          <div className="relative w-[148px]">
-            <Image
-              src={이미지}
-              alt="cover"
-              className="h-full w-full object-center"
-              fill
-              placeholder="blur"
-              blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg=="
-            />
-          </div>
-          <div className="flex h-full w-full gap-8 p-12">
-            <div className="flex w-[248px] flex-col gap-8">
-              <div className="flex items-center gap-8 text-grayscale-300 detail2-semibold">
-                {(() => {
-                  switch (플랫폼) {
-                    case 'fastcampus':
-                      return <TooltipIcon variant="fastcampus" size="16" />;
-                    case 'inflearn':
-                      return <TooltipIcon variant="inflearn" size="16" />;
-                    case 'coloso':
-                      return <TooltipIcon variant="coloso" size="16" />;
-                    case 'class101':
-                      return <TooltipIcon variant="class101" size="16" />;
-                    default:
-                      return null;
-                  }
-                })()}
-                {강사명}
-              </div>
-              <div className="body2-semibold">{강의명}</div>
+const LectureDialog = React.forwardRef<React.ElementRef<typeof DialogPrimitive.Root>, LectureDialogProps>(
+  (
+    {
+      강사명 = '둡',
+      강의명 = '연필 하나만으로 모든 분위기를 담아내요, 둡의 연필 드로잉',
+      가격 = '100,000원',
+      플랫폼 = 'fastcampus',
+      이미지 = '',
+      별점 = 4.5,
+      리뷰수 = 100,
+      추천후기 = [],
+      children,
+      contentProps,
+      ...props
+    },
+    ref
+  ) => {
+    return (
+      <Dialog {...props}>
+        <DialogTrigger asChild>{children}</DialogTrigger>
+        <DialogContent className="z-10 flex h-[534px] w-[837px] flex-col bg-gray-100" ref={ref} {...contentProps}>
+          <DialogHeader className="flex h-[91px] w-full bg-white">
+            <div className="relative w-[148px]">
+              <Image
+                src={이미지}
+                alt="cover"
+                className="h-full w-full object-center"
+                fill
+                placeholder="blur"
+                blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFklEQVR42mN8//HLfwYiAOOoQvoqBABbWyZJf74GZgAAAABJRU5ErkJggg=="
+              />
             </div>
-            <div className="flex items-end body3-extrabold">{가격}</div>
-            <div className="flex items-end gap-8">
-              <div>
-                <Button className="flex items-center gap-[4px]" size="sm" variant="outlined">
-                  후기 남기기
-                  <PencilIcon className="h-16 w-16" />
-                </Button>
+            <div className="flex h-full w-full gap-8 p-12">
+              <div className="flex w-[248px] flex-col gap-8">
+                <div className="flex items-center gap-8 text-grayscale-300 detail2-semibold">
+                  {(() => {
+                    switch (플랫폼) {
+                      case 'fastcampus':
+                        return <TooltipIcon variant="fastcampus" size="16" />;
+                      case 'inflearn':
+                        return <TooltipIcon variant="inflearn" size="16" />;
+                      case 'coloso':
+                        return <TooltipIcon variant="coloso" size="16" />;
+                      case 'class101':
+                        return <TooltipIcon variant="class101" size="16" />;
+                      default:
+                        return null;
+                    }
+                  })()}
+                  {강사명}
+                </div>
+                <div className="line-clamp-2 body2-semibold">{강의명}</div>
               </div>
-              <div>
-                <Button className="flex items-center gap-[4px]" size="sm" variant="outlined">
-                  사이트로 이동
-                  <MoveDownRightIcon className="h-16 w-16" />
-                </Button>
-              </div>
-              <div>
-                <Button className="flex items-center gap-[4px]" size="sm" variant="outlined">
-                  강의 찜하기
-                  <Bookmark className="h-16 w-16" />
-                </Button>
-              </div>
-            </div>
-          </div>
-        </DialogHeader>
-        <div className="flex h-full flex-col gap-16 overflow-hidden px-20 py-20">
-          <div className="flex w-full justify-center">
-            <div className="flex flex-col items-center">
-              <div className="H4-bold">{별점}</div>
-              <div className="text-grayscale-400 detail2-semibold">{`총 ${리뷰수}개의 리뷰`}</div>
-              <div>
-                <Rating
-                  emptyStyle={{ display: 'flex' }}
-                  fillStyle={{ display: '-webkit-inline-box' }}
-                  initialValue={별점}
-                  size={24}
-                  allowFraction
-                  readonly
-                />
+              <div className="flex items-end body3-extrabold">{가격}</div>
+              <div className="flex items-end gap-8">
+                <div>
+                  <Button className="flex items-center gap-[4px]" size="sm" variant="outlined">
+                    후기 남기기
+                    <PencilIcon className="h-16 w-16" />
+                  </Button>
+                </div>
+                <div>
+                  <Button className="flex items-center gap-[4px]" size="sm" variant="outlined">
+                    사이트로 이동
+                    <MoveDownRightIcon className="h-16 w-16" />
+                  </Button>
+                </div>
+                <div>
+                  <Button className="flex items-center gap-[4px]" size="sm" variant="outlined">
+                    강의 찜하기
+                    <Bookmark className="h-16 w-16" />
+                  </Button>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="flex w-full gap-16">
-            <div className="flex flex-col">
-              <div className="flex items-center gap-8 detail1-bold">
-                <Like /> 많은 추천을 받은 후기
+          </DialogHeader>
+          <div className="flex h-full flex-col gap-16 overflow-hidden px-20 py-20">
+            <div className="flex w-full justify-center">
+              <div className="flex flex-col items-center">
+                <div className="H4-bold">{별점}</div>
+                <div className="text-grayscale-400 detail2-semibold">{`총 ${리뷰수}개의 리뷰`}</div>
+                <div>
+                  <Rating
+                    emptyStyle={{ display: 'flex' }}
+                    fillStyle={{ display: '-webkit-inline-box' }}
+                    initialValue={별점}
+                    size={24}
+                    allowFraction
+                    readonly
+                  />
+                </div>
               </div>
-              <div className="flex w-[360px] flex-col items-center justify-center">
-                {추천후기 && 추천후기.length > 0 ? (
-                  <ScrollArea className="h-[300px] pt-12">
-                    {추천후기.map((review) => (
-                      <HorizontalCard className="mb-8" variant="lecture" key={review.작성자} {...review} />
-                    ))}
-                  </ScrollArea>
-                ) : (
-                  <div className=" gap-8 text-center body3-semibold">
-                    <Notification />
-                    아직 작성된 후기가 없어요
-                    <br />
-                    후기가 등록되면 알려드릴게요!
+            </div>
+            <div className="flex w-full gap-16">
+              <div className="flex flex-col">
+                <div className="flex items-center gap-8 detail1-bold">
+                  <Like /> 많은 추천을 받은 후기
+                </div>
+                <div className="flex h-full w-[360px] flex-col items-center justify-center">
+                  {추천후기 && 추천후기.length > 0 ? (
+                    <ScrollArea className="h-[300px] pt-12">
+                      {추천후기.map((review) => (
+                        <HorizontalCard className="mb-8" variant="lecture" key={review.작성자} {...review} />
+                      ))}
+                    </ScrollArea>
+                  ) : (
+                    <div className="flex h-full flex-col items-center justify-center gap-8 text-center body3-semibold">
+                      <Notification />
+                      아직 작성된 후기가 없어요
+                      <br />
+                      후기가 등록되면 알려드릴게요!
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div className="flex w-full flex-col gap-12">
+                <div className="flex w-full gap-16">
+                  <div className="flex w-full flex-col">
+                    <div className="flex items-center gap-8 pb-12 detail1-bold">
+                      <Bookmark2 /> 강의 내용은 이러해요
+                    </div>
+                    <div className="flex flex-col gap-[4px]">
+                      <Progress variant="primary_1" label="커리큘럼과 똑같아요" count={9} value={80}>
+                        Progress
+                      </Progress>
+                      <Progress variant="primary_2" label="구성이 알차요" count={9} value={60}>
+                        Progress
+                      </Progress>
+                      <Progress variant="primary_3" label="내용이 자세해요" count={9} value={50}>
+                        Progress
+                      </Progress>
+                      <Progress variant="primary_4" label="이해가 잘돼요" count={9} value={30}>
+                        Progress
+                      </Progress>
+                    </div>
                   </div>
-                )}
-              </div>
-            </div>
-            <div className="flex w-full flex-col gap-12">
-              <div className="flex w-full gap-16">
-                <div className="flex w-full flex-col">
-                  <div className="flex items-center gap-8 pb-12 detail1-bold">
-                    <Bookmark2 /> 강의 내용은 이러해요
-                  </div>
-                  <div className="flex flex-col gap-[4px]">
-                    <Progress variant="primary_1" label="커리큘럼과 똑같아요" count={9} value={80}>
-                      Progress
-                    </Progress>
-                    <Progress variant="primary_2" label="구성이 알차요" count={9} value={60}>
-                      Progress
-                    </Progress>
-                    <Progress variant="primary_3" label="내용이 자세해요" count={9} value={50}>
-                      Progress
-                    </Progress>
-                    <Progress variant="primary_4" label="이해가 잘돼요" count={9} value={30}>
-                      Progress
-                    </Progress>
+                  <div className="flex w-full flex-col">
+                    <div className="flex items-center gap-8 pb-12 detail1-bold">
+                      <Career /> 강사님은 이러해요
+                    </div>
+                    <div className="flex flex-col gap-[4px]">
+                      <Progress variant="secondary_1" label="커리큘럼과 똑같아요" count={9} value={80}>
+                        Progress
+                      </Progress>
+                      <Progress variant="secondary_2" label="구성이 알차요" count={9} value={60}>
+                        Progress
+                      </Progress>
+                      <Progress variant="secondary_3" label="내용이 자세해요" count={9} value={50}>
+                        Progress
+                      </Progress>
+                      <Progress variant="secondary_4" label="이해가 잘돼요" count={9} value={30}>
+                        Progress
+                      </Progress>
+                    </div>
                   </div>
                 </div>
                 <div className="flex w-full flex-col">
                   <div className="flex items-center gap-8 pb-12 detail1-bold">
-                    <Career /> 강사님은 이러해요
+                    <Career /> 강의 후, 나의 변화는?
                   </div>
                   <div className="flex flex-col gap-[4px]">
-                    <Progress variant="secondary_1" label="커리큘럼과 똑같아요" count={9} value={80}>
+                    <Progress variant="blue_1" label="커리큘럼과 똑같아요" count={9} value={80}>
                       Progress
                     </Progress>
-                    <Progress variant="secondary_2" label="구성이 알차요" count={9} value={60}>
+                    <Progress variant="blue_2" label="구성이 알차요" count={9} value={60}>
                       Progress
                     </Progress>
-                    <Progress variant="secondary_3" label="내용이 자세해요" count={9} value={50}>
-                      Progress
-                    </Progress>
-                    <Progress variant="secondary_4" label="이해가 잘돼요" count={9} value={30}>
+                    <Progress variant="blue_3" label="내용이 자세해요" count={9} value={50}>
                       Progress
                     </Progress>
                   </div>
-                </div>
-              </div>
-              <div className="flex w-full flex-col">
-                <div className="flex items-center gap-8 pb-12 detail1-bold">
-                  <Career /> 강의 후, 나의 변화는?
-                </div>
-                <div className="flex flex-col gap-[4px]">
-                  <Progress variant="blue_1" label="커리큘럼과 똑같아요" count={9} value={80}>
-                    Progress
-                  </Progress>
-                  <Progress variant="blue_2" label="구성이 알차요" count={9} value={60}>
-                    Progress
-                  </Progress>
-                  <Progress variant="blue_3" label="내용이 자세해요" count={9} value={50}>
-                    Progress
-                  </Progress>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </DialogContent>
-    </Dialog>
-  );
-}
+        </DialogContent>
+      </Dialog>
+    );
+  }
+);
+
+LectureDialog.displayName = 'LectureDialog';
 
 export default LectureDialog;
